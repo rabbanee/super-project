ef4a41e5bb856749a74215d6008b13ee1993a573
rabbanee <daffarabbanee@gmail.com>
2021-01-02 22:23:23 +0700

    Setup project

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import 'tailwindcss/tailwind.css';
+import PropTypes from 'prop-types';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <button className="py-2 dark:text-black px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+          Click me
+      </button>
+     </Fragment>
+  );
+}
