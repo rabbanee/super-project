1d4c027c67430df05e02ad5d4c70282c53776b54
dodit1156 <dodit1156@gmail.com>
2021-02-22 13:27:08 +0700

    1123245

--- src/components/templates/exams/ExamForTeacher.tsx
+++ src/components/templates/exams/ExamForTeacher.tsx
@@ -19,6 +19,8 @@ import { useRef, useState } from 'react';
 import Modal from '@elements/Modal';
 import ModalBody from '@elements/ModalBody';
 import ModalFooter from '@elements/ModalFooter';
+import grades from '@data/grades';
+import dynamic from 'next/dynamic'
 import ContainerFooter from '@elements/container/Footer';
 import * as OutlineIcon from '@elements/icon/Outline';
 
@@ -27,6 +29,11 @@ interface ExamForTeacherProps {
   user: User,
   title: string,
 }
+
+const Editor = dynamic(
+  () => import('@modules/Editor'),
+  { ssr: false }
+)
 
 
 const ExamForTeacher = ({ user }) => {
@@ -37,6 +44,7 @@ const ExamForTeacher = ({ user }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isModalShow, setIsModalShow] = useState(false);
   const chapterNameRef = useRef();
+  const [selectedGrade, setSelectedGrade] = useState(grades[0]);
 
   return (
     <>
@@ -44,74 +52,9 @@ const ExamForTeacher = ({ user }) => {
       <LayoutWithSidebar user={user} title="Ujian">
         <Container>
           <ContainerBody className="rounded-b-xl">
-          <form>
-            <Container>
-              <ContainerBody className="px-4 py-5 bg-white sm:p-6 rounded-t-xl">
-                <div className="flex justify-between mb-2">
-                  <h2 className="text-4xl font-bold	text-black mb-2"></h2>
-                  <Button.Primary type="button" onClick={() => setIsModalShow(true)} className="inline-flex items-center">
-                    <SolidIcon.Plus className="-ml-1 mr-1 h-5 w-5" />
-                    Tambah Ujian
-                  </Button.Primary>
-                </div>
-                <div className="grid grid-cols-6 gap-4 mt-2">
-                  <div className="col-span-6 sm:col-span-6">
-                    <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Urutan Materi</label>
-                    <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" placeholder="Urutan Materi"/>
-                  </div>
-                  <div className="col-span-6 sm:col-span-6">
-                    <ListBox items={dummySubjects} label="Mata Pelajaran" selectedItem={selectedSubject} setSelectedItem={setSelectedSubject}/>
-                  </div>
-                  <div className="col-span-6 sm:col-span-6">
-                    <ListBox items={dummyChapters} label="Bab" selectedItem={selectedChapter} setSelectedItem={setSelectedChapter}/>
-                  </div>
-                </div>
-              </ContainerBody>
-              <ContainerFooter className="flex justify-end">
-                <Button.Primary  
-                  className={`${isLoading && 'cursor-not-allowed'} inline-flex items-center`}
-                  disabled={isLoading}
-                >
-                    {
-                      isLoading && <OutlineIcon.Circle className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" /> 
-                    }
-                    {
-                      isLoading ? 'Memproses' : <><SolidIcon.Save className="-ml-1 mr-1 h-5 w-5" /> Simpan</>
-                    }
-                </Button.Primary>
-              </ContainerFooter>
-            </Container>
-           </form>
-           <Modal isShow={isModalShow} setIsShow={setIsModalShow}>
-            <form>
-              <ModalBody>
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
-                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                      Tambahkan Bab
-                    </h3>
-                    <div className="mt-4 flex flex-col space-y-3">   
-                      <div>
-                        <ListBox items={dummySubjects} label="Mata Pelajaran" selectedItem={selectedSubject} setSelectedItem={setSelectedSubject}/>
-                      </div>
-                      <div>
-                        <label htmlFor="chapter" className="block text-sm font-medium text-gray-700">Nama Bab</label>
-                        <input id="chapter" name="chapter" type="text" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" ref={chapterNameRef} placeholder="Nama Bab" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button.Primary className="w-full inline-flex justify-center shadow-sm text-base font-medium sm:ml-3 sm:w-auto sm:text-sm">
-                  Tambahkan Bab
-                </Button.Primary>
-                <Button.Secondary type="button" className="mt-3 w-full inline-flex justify-center shadow-sm text-base font-medium sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={() => setIsModalShow(false)}>
-                  Batal
-                </Button.Secondary>
-              </ModalFooter>
-            </form>
-          </Modal>
+          <div className="flex items-end flex-col mb-2">
+            <Button.Primary onClick={() => setIsModalShow(true)}>Tambahkan Ujian</Button.Primary>
+          </div>
             <div className="flex justify-between space-y-3">
               <div className="flex justify-center items-center self-end space-x-1">
                 <span className="text-md">Tampilkan</span>
@@ -164,6 +107,84 @@ const ExamForTeacher = ({ user }) => {
                   </Td>
                 </tr>
               </tbody>
+              <Modal isShow={isModalShow} setIsShow={setIsModalShow}>
+            <form>
+              <ModalBody>
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:text-left w-full">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                      Tambahkan Ujian
+                    </h3>
+                    <div className="mt-4 flex flex-col space-y-3">   
+                      <div>
+                        <td>
+                        <ListBox items={dummySubjects} label="Class" selectedItem={selectedSubject} setSelectedItem={setSelectedSubject}/>
+                        </td>
+                        <td>
+                        <ListBox items={dummySubjects} label="Section" selectedItem={selectedSubject} setSelectedItem={setSelectedSubject}/>
+                        </td> 
+                        <td>
+                        <ListBox items={dummySubjects} label="Subject" selectedItem={selectedSubject} setSelectedItem={setSelectedSubject}/>
+                        </td>
+                        
+                      </div>
+                      <div>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Title</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                      </div>
+                      <div>
+                        <Editor/>
+                      </div>
+                      <div className="col-span-6 sm:col-span-6">
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Exam Start</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Start Time</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                      </div>
+                      <div className="col-span-6 sm:col-span-6">
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Exam End</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Time over</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                      </div>
+                      <div className="col-span-6 sm:col-span-6">
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Total Question</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Duration(minutes)</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                      </div>
+                      <div className="col-span-6 sm:col-span-6">
+                        <td>
+                        <label htmlFor="order_of_the_material" className="block text-sm font-medium text-gray-700">Minimum Test Score</label>
+                        <input id="order_of_the_material" name="order_of_the_material" type="text" autoComplete="order_of_the_material" required className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-primary-dark focus:border-primary-dark focus:z-10 sm:text-sm" />
+                        </td>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                    <Button.Primary className="w-full inline-flex justify-center shadow-sm text-base font-medium sm:ml-3 sm:w-auto sm:text-sm">
+                      Tambahkan Pengumuman
+                    </Button.Primary>
+                    <Button.Secondary type="button" className="mt-3 w-full inline-flex justify-center shadow-sm text-base font-medium sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={() => setIsModalShow(false)}>
+                      Batal
+                    </Button.Secondary>
+                  </ModalFooter>
+                </form>
+              </Modal>
             </Table>
             <Pagination />
           </ContainerBody>
